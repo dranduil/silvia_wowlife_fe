7f153f46991fe3bdb2bab20b3cb9ffebb3126906
Steeven Lacerna <steeven93@icloud.com>
2022-03-13 16:45:50 +0100

    #configvue

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
     // publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
     publicPath: '/',
-    productionSourceMap: false
+    productionSourceMap: false,
+    assetsDir: 'assets',
+    indexPath: 'templates/index.html'
 }
